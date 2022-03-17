7ef1c9c3afbcb140a8d7644a1299bd35cf825bab
Isaias D <isaiasd.garciar@gmail.com>
2022-03-16 20:56:10 -0600

    add strategy patter

---
+++ src/strategy_pattern/example.py
@@ -0,0 +1,23 @@
+def executor(arg1, arg2, func=None):
+    if func is None:
+        return "Strategy not implemented"
+
+    return func(arg1, arg2)
+
+
+def strategy_addition(arg1, arg2):
+    return arg1 + arg2
+
+
+def strategy_subtraction(arg1, arg2):
+    return arg1 - arg2
+
+
+def main():
+    print(executor(4, 2))
+    print(executor(4, 2, strategy_addition))
+    print(executor(4, 2, strategy_subtraction))
+
+
+if __name__ == "__main__":
+    main()
